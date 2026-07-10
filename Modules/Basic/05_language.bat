c61a6b456aa1f9bba1662b8c51d585516daeca89
Kuka001 <kuanysh.azimbek@gmail.com>
2026-07-11 00:33:17 +0500

    Update project files after local changes

--- Modules/Basic/05_language.bat
+++ Modules/Basic/05_language.bat
@@ -24,9 +24,9 @@ if "%c%"=="2" (
 )
 if "%c%"=="3" (
     cls & echo Настройка комбинированной раскладки...
-    powershell -NoProfile -Command "$L=Get-WinUserLanguageList;$L=@($L|?{$_.LanguageTag-notmatch'^kk'});$R=$L|?{$_.LanguageTag-match'^ru'};if(-not $R){$R=(New-WinUserLanguageList 'ru-RU')[0];$L+=$R};if(-not $R.InputMethodTips.Contains('0419:0000043F')){$R.InputMethodTips.Add('0419:0000043F')};Set-WinUserLanguageList $L -Force" >nul 2>&1
+    powershell -NoProfile -Command "$L=Get-WinUserLanguageList; $R=$L.Where({$_.LanguageTag -match '^ru'}); if($R){$R=$R[0]}; $layoutExists=$R -and $R.InputMethodTips.Contains('0419:0000043F'); $hotkeyExists=(Get-ItemProperty -Path 'HKCU:\Keyboard Layout\Toggle' -ErrorAction SilentlyContinue).'Layout Hotkey' -eq '2'; if($layoutExists){ Write-Host '%Y%Русская + Казахская раскладка уже существует внутри Русского языка.%X%'; if(-not $hotkeyExists){ Write-Host '%G%Переключение раскладки (Ctrl+Shift) успешно настроено.%X%' }; Write-Host '%G%Для смены раскладки используйте: Ctrl+Shift%X%' } else { $L=@($L.Where({$_.LanguageTag -notmatch '^kk'})); if(-not $R){ $R=(New-WinUserLanguageList 'ru-RU')[0]; $L+=$R; $s='успешно создана с нуля.' } else { $s='успешно добавлена в Русскую раскладку.' }; if(-not $R.InputMethodTips.Contains('0419:00000419')) { $R.InputMethodTips.Add('0419:00000419') }; if(-not $R.InputMethodTips.Contains('0419:0000043F')) { $R.InputMethodTips.Add('0419:0000043F') }; Set-WinUserLanguageList $L -Force; Write-Host ('%G%Русская + Казахская раскладка ' + $s + '%X%'); if(-not $hotkeyExists){ Write-Host '%G%Переключение раскладки (Ctrl+Shift) успешно настроено.%X%' }; Write-Host '%G%Для смены раскладки используйте: Ctrl+Shift%X%' }"
     reg add "HKCU\Keyboard Layout\Toggle" /v "Language Hotkey" /t REG_SZ /d "1" /f >nul 2>&1
     reg add "HKCU\Keyboard Layout\Toggle" /v "Layout Hotkey" /t REG_SZ /d "2" /f >nul 2>&1
-    echo %G%Готово!%X% & echo. & pause & goto language_menu
+    echo. & pause & goto language_menu
 )
 goto language_menu
